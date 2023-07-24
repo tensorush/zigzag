9f1257054577d2ff1face38a1f98bcde15bf613e
Jora Troosh <tensorush@gmail.com>
2023-07-24 15:56:45 +0300

    Patch

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *std.Build) void {
         .root_source_file = root_source_file,
         .target = b.standardTargetOptions(.{}),
         .optimize = b.standardOptimizeOption(.{}),
-        .version = .{ .major = 1, .minor = 2, .patch = 0 },
+        .version = .{ .major = 1, .minor = 2, .patch = 1 },
     });
     b.installArtifact(zigzag);
 
